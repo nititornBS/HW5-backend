c1392d73623c0267ec96e03a08ee50236b08b29f
Than <tan95685@gmail.com>
2023-11-19 20:38:42 +0700

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // HOST: "localhost",
+  // USER: "root",
+  // PASSWORD: "",
+  // DB: "test",
+  HOST: "sql.freedb.tech",
+  USER: "freedb_TanaT",
+  PASSWORD: "P8G%&cZucyu2km9",
+  DB: "freedb_manage_user",
+};
+
