991cf28a57294f2364e51af113c92e4682e9323a
Than <tan95685@gmail.com>
2023-11-19 21:46:02 +0700

    fix upload img again

--- app/middleware/upload.js
+++ app/middleware/upload.js
@@ -5,9 +5,9 @@ const storage = multer.diskStorage({
         cb(null, __basedir + "/assets/");
     },
     filename: (req, file, cb)=>{
-        // const extArray = file.mimetype.split("/");
-        // const extension = extArray[extArray.length-1];
-        const newFilename = "Fileupload";
+        const extArray = file.mimetype.split("/");
+        const extension = extArray[extArray.length-1];
+        const newFilename = "Fileupload-"+Date.now()+"."+extension;
         cb(null, newFilename);
     }
 });
